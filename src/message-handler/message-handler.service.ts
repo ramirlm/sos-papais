9b9668baa959e93fe8a9758f70dd67c900f64952
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-08-22 15:35:21 -0300

    feat: handle parent register

--- src/message-handler/message-handler.service.ts
+++ src/message-handler/message-handler.service.ts
@@ -1,6 +1,7 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { OllamaAiService } from '../ollama-ai/ollama-ai.service';
 import { WhatsappWebService } from '../whatsapp-web/whatsapp-web.service';
+import { ParentsService } from '../parents/parents.service';
 
 @Injectable()
 export class MessageHandlerService {
@@ -8,13 +9,35 @@ export class MessageHandlerService {
     @Inject(forwardRef(() => WhatsappWebService))
     private readonly whatsappClient: WhatsappWebService,
     private readonly ollamaAiService: OllamaAiService,
+    private readonly parentsService: ParentsService,
   ) {}
 
   async handleIncomingMessage(incomingMessage: { from: string; body: string }) {
     const { from, body } = incomingMessage;
     const userPhoneNumber = from.replace('whatsapp:', '');
 
+    const parent = await this.parentsService.findByPhone(userPhoneNumber);
+
+    if (!parent) return this.handleNotFoundParent(userPhoneNumber);
+    if (!parent.name) return this.handleParentName(userPhoneNumber, body);
+
     const response = await this.ollamaAiService.generateResponse(body);
     await this.whatsappClient.sendWhatsAppMessage(userPhoneNumber, response);
+  }
+
+  async handleNotFoundParent(phoneNumber: string) {
+    this.parentsService.register(phoneNumber);
+    this.whatsappClient.sendWhatsAppMessage(
+      phoneNumber,
+      'Olá! Seja bem-vindo ao SOS Papais, parece que você é novo por aqui, qual o seu nome?',
+    );
+  }
+
+  async handleParentName(phoneNumber: string, name: string) {
+    await this.parentsService.updateName(phoneNumber, name);
+    this.whatsappClient.sendWhatsAppMessage(
+      phoneNumber,
+      `Obrigado por compartilhar seu nome, ${name}! Como posso ajudar você hoje?`,
+    );
   }
 }
