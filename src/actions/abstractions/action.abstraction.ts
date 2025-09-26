395437394524f14883216ff10d4a8d9504f4b060
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-09-26 13:23:56 -0300

    feat: improve action abstraction

--- src/actions/abstractions/action.abstraction.ts
+++ src/actions/abstractions/action.abstraction.ts
@@ -1,3 +1,5 @@
-export abstract class Action {
-    abstract execute(): { response: string, showMenuOnFinish: boolean };
+import { CompletedAction } from "../interfaces/completed-action.interface";
+
+export abstract class Action<T = {}> {
+    abstract execute(props: T): Promise<CompletedAction>;
 }
